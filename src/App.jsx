ff8a693b1e7d35ba3dc39900e868d1e97ac8d405
Jackal <mohdsaifuddin9794@gmail.com>
2025-06-05 21:49:03 +0530

    complete crud using context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,59 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import { TodoProvider } from './context'
-import AddForm from './components/AddForm'
-import TodoItems from './components/TodoItems'
+import { TodoProvider } from "./context";
+import AddForm from "./components/AddForm";
+import TodoItems from "./components/TodoItems";
 
 function App() {
   const [todos, setTodos] = useState([]);
-    const {isEdit, setIsEdit} = useState(null);
-    const [editTxt, setEditTxt] = useState("")
+  const [isEdit, setIsEdit ] = useState(null);
+  const [editTxt, setEditTxt] = useState("");
   const addTodo = (todo) => {
-    const newTodo = [{id: Date.now(), todo, complete: false}, ...todos]
+    const newTodo = [{ id: Date.now(), todo, complete: false }, ...todos];
     setTodos(newTodo);
     // console.log(newTodo);
-  }
+  };
   const deleteTodo = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id))
-  }
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
   const toggleComplete = (id) => {
-    console.log(id)
-    setTodos(todo => todo.map(
-      (prevTodo)=> prevTodo.id === id
-       ? {...prevTodo, complete:!prevTodo.complete}
-        : prevTodo));
-  }
+    console.log(id);
+    setTodos((todo) =>
+      todo.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, complete: !prevTodo.complete }
+          : prevTodo
+      )
+    );
+  };
   const startEdit = (id) => {
-    const selectedId = todos.find(todo => todo.id === id);
-    // setIsEdit(id);
-    setEditTxt(selectedId.todo); // assuming `task` is the text
-    console.log(selectedId.todo);
-  }
-  
+    const selectedId = todos.find((todo) => todo.id === id);
+    if(selectedId){
+
+      setIsEdit(id);
+      setEditTxt(selectedId.todo); // assuming `task` is the text
+      console.log(selectedId.todo);
+    }
+  };
+const updateTodo = () => {
+  setTodos(prevTodo => prevTodo.map(todo => todo.id === isEdit ? {...todo, todo:editTxt} : todo))
+  setIsEdit(null);
+  setEditTxt("")
+}
   return (
     <>
-          <TodoProvider value={{todos, addTodo, deleteTodo, toggleComplete, startEdit}}>
-              <AddForm  />
-              {
-
-                todos.map(( todo) => 
-                <div key={todo.id}>
-
-                  <TodoItems todo={todo}></TodoItems>
-                </div>)
-              }
-          </TodoProvider>
+      <TodoProvider
+        value={{ todos, addTodo, deleteTodo, toggleComplete, startEdit, updateTodo }}
+      >
+        <AddForm isEdit={isEdit} editTxt={editTxt} setEditTxt={setEditTxt} />
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <TodoItems todo={todo}></TodoItems>
+          </div>
+        ))}
+      </TodoProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,22 +1,41 @@
 import React, { useContext, useState } from 'react';
 import { TodoContext, useTodo } from '../context';
 
-const AddForm = () => {
-    const [todo, setTodo] = useState("");
-    const {addTodo} = useTodo();
-const handleSubmit = (e) =>{
-    e.preventDefault()
-addTodo(todo);
-setTodo("")
-}
-    return (
-        <div>
-            <form onSubmit={handleSubmit} action="" className='flex items-center gap-2'>
-                <input type="text" className="text-lg bg-gray-800 text-white p-3 rouded-lg outline-0" value={todo} onChange={e => setTodo(e.target.value)} />
-                <button className="p-3 rounded-lg text-white bg-blue-500">Add</button>
-            </form>
-        </div>
-    );
-}
+const AddForm = ({ isEdit, editTxt, setEditTxt   }) => {
+  const [todo, setTodo] = useState("");
+  const { addTodo, updateTodo } = useTodo();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isEdit !== null) {
+      updateTodo();
+    } else {
+      addTodo(todo);
+      setTodo("");
+    }
+  };
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        action=""
+        className="flex items-center gap-2"
+      >
+        <input
+          type="text"
+          className="text-lg bg-gray-800 text-white p-3 rouded-lg outline-0"
+          value={isEdit !== null ? editTxt : todo}
+          onChange={(e) =>
+            isEdit !== null
+              ? setEditTxt(e.target.value)
+              : setTodo(e.target.value)
+          }
+        />
+        <button className="p-3 rounded-lg text-white bg-blue-500">
+          {isEdit !== null ? "Update" : "Add"}
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default AddForm;
